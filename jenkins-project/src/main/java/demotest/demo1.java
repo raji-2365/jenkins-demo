2677bb0bf03dc06801d0359f3c82366e8b1d9a13
RAJYALAXMI REDDY <RAJYALAXMI REDDY@LAPTOP-IJ0C13NH>
2023-01-07 00:31:37 +0530

    first message

---
+++ jenkins-project/src/main/java/demotest/demo1.java
@@ -0,0 +1,13 @@
+package demotest;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo1 {
+	@Test
+	public void m1() {
+		Reporter.log("m1 running");
+	}
+
+}
---
+++ jenkins-project/src/main/java/demotest/demo2.java
@@ -0,0 +1,13 @@
+package demotest;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo2 {
+	@Test
+	public void m2() {
+		Reporter.log("m2 running");
+	}
+
+}
